4aa6685db32531355fa4661f0b07ae094e5c202d
Vasily <v.sliouniaev@gmail.com>
2019-05-09 12:26:35 +0100

    Vendor
    Signed-off-by: Vasily <v.sliouniaev@gmail.com>

--- vendor/github.com/improbable-eng/thanos/doc.go
+++
@@ -1,6 +0,0 @@
-// Package thanos is a set of components that
-// can provide highly available Prometheus
-// setup with long term storage capabilities.
-//
-// See https://github.com/improbable-eng/thanos/blob/master/docs/getting_started.md for first steps.
-package thanos // import "github.com/improbable-eng/thanos"
